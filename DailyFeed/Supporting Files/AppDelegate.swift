7a7b376eaa029b03b4780b366f47f7450a2a334e
A guy <aguy@gmail.com>
2019-12-17 22:38:41 +0700

    Update cache migration

--- DailyFeed/Supporting Files/AppDelegate.swift
+++ DailyFeed/Supporting Files/AppDelegate.swift
@@ -56,62 +56,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let config = Realm.Configuration(
             // Set the new schema version. This must be greater than the previously used
             // version (if you've never set a schema version before, the version is 0).
-            schemaVersion: 9,
+            schemaVersion: UInt64(CacheVersion.CURRENT_VERSION.rawValue),
             
             // Set the block which will be called automatically when opening a Realm with
             // a schema version lower than the one set above
             migrationBlock: { migration, oldSchemaVersion in
                 // We haven’t migrated anything yet, so oldSchemaVersion == 0
-                if (oldSchemaVersion < 1) {
-                    print("smaller than 1")
-                }
-                
-                if (oldSchemaVersion < 2) {
-                    print("smaller than 2")
-                }
-                
-                if (oldSchemaVersion < 3) {
-                    print("smaller than 3")
-                }
-                
-                if (oldSchemaVersion < 4){
-                    print("smaller than 4")
-                }
-                if (oldSchemaVersion < 9){
-                    print("smaller than 9")
-                    var urlBookmarks = [String]()
-                    migration.enumerateObjects(ofType: DailyFeedRealmModel.className(), { (old, newBook) in
-                        guard let old = old else {
-                            return
-                        }
-                        
-                        guard let bookmarkUrl = old["url"] as? String else{
-                            return
-                        }
-                        urlBookmarks.append(bookmarkUrl)
-                    })
-                    
-                    var testNewList = [MigrationObject]()
-                    migration.enumerateObjects(ofType: ArticleTestRealmModel.className(), { (old, new
-                        ) in
-                        guard let new = new else {
-                            return
-                        }
-                        testNewList.append(new)
-                    })
-                    
-                    print("urlBookmark size = \(urlBookmarks.count)")
-                    print("testNewList size = \(testNewList.count)")
-                    testNewList = testNewList.filter({ (item) -> Bool in
-                        guard let articleUrl = item["url"] as? String else {
-                            return false
-                        }
-                        return urlBookmarks.contains(articleUrl)
-                    })
-                    testNewList.forEach({ (item) in
-                        item["isBookmark"] = true
-                    })
-                }
+            let manager = MigrationManager(migration: migration, oldVersion: Int64(oldSchemaVersion))
+            manager.handle()
         })
         
         // Tell Realm to use this new configuration object for the default Realm
