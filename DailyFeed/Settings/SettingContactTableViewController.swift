acb891105b104b0f558ce2577a98f65c746f04be
A guy <aguy@gmail.com>
2020-02-16 20:04:53 +0700

    Embed icon in text

--- DailyFeed/Settings/SettingContactTableViewController.swift
+++ DailyFeed/Settings/SettingContactTableViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class SettingContactTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var lbSubcriber: UILabel!
+    @IBOutlet weak var lbNorthAmericanPhoneNumber: UILabel!
+    @IBOutlet weak var lbNorthAmericanEmail: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         tableView.dataSource = self
         tableView.delegate = self
         tableView.tableFooterView = UIView(frame: CGRect.zero)
+        
+        lbSubcriber.font = UIFont.boldSystemFont(ofSize: 17.0)
+        lbNorthAmericanPhoneNumber.embedIcon(image: UIImage(named: "close")!)
+        lbNorthAmericanEmail.embedIcon(image: UIImage(named: "bookmark")!)
+        let image = UIApplicationShortcutIcon(type: .mail)
     }
 }
