91b4ce7e1644ab3915a6e795b7828423d100a2ea
A guy <aguy@gmail.com>
2020-02-23 17:38:26 +0700

    Add localize params for short time

--- DailyFeed/Supporting Files/Date+Relative.swift
+++ DailyFeed/Supporting Files/Date+Relative.swift
@@ -24,35 +24,35 @@ extension Date {
                                                                      to: now)
 
         if let years = components.year, years > 0 {
-            return short ? "\(years)y" : "\(years) year\(years == 1 ? "" : "s") ago"
+            return short ? "%dy".localizeWithFormat(arguments: years) : "\(years) year\(years == 1 ? "" : "s") ago"
         }
 
         if let months = components.month, months > 0 {
-            return short ? "\(months)mo" : "\(months) month\(months == 1 ? "" : "s") ago"
+            return short ? "%dmo".localizeWithFormat(arguments: months) : "\(months) month\(months == 1 ? "" : "s") ago"
         }
 
         if let weeks = components.weekOfYear, weeks > 0 {
-            return short ? "\(weeks)w" : "\(weeks) week\(weeks == 1 ? "" : "s") ago"
+            return short ? "%dweeks".localizeWithFormat(arguments: weeks) : "\(weeks) week\(weeks == 1 ? "" : "s") ago"
         }
         if let days = components.day, days > 0 {
-            guard days > 1 else { return short ? "  y'day" : "yesterday" }
+            guard days > 1 else { return short ? "  y'day".localized : "yesterday".localized }
 
-            return short ? "\(days)d" : "\(days) day\(days == 1 ? "" : "s") ago"
+            return short ? "%dd".localizeWithFormat(arguments: days) : "\(days) day\(days == 1 ? "" : "s") ago"
         }
 
         if let hours = components.hour, hours > 0 {
-            return short ? "\(hours)h" : "\(hours) hour\(hours == 1 ? "" : "s") ago"
+            return short ? "%dh".localizeWithFormat(arguments: hours) : "\(hours) hour\(hours == 1 ? "" : "s") ago"
         }
 
         if let minutes = components.minute, minutes > 0 {
-            return short ? "\(minutes) min" : "\(minutes) minute\(minutes == 1 ? "" : "s") ago"
+            return short ? "%d min".localizeWithFormat(arguments: minutes) : "\(minutes) minute\(minutes == 1 ? "" : "s") ago"
         }
 
         if let seconds = components.second, seconds > 30 {
-            return short ? "\(seconds)s" : "\(seconds) second\(seconds == 1 ? "" : "s") ago"
+            return short ? "%ds".localizeWithFormat(arguments: seconds) : "\(seconds) second\(seconds == 1 ? "" : "s") ago"
         }
 
-        return "Just now"
+        return "Just now".localized
     }
 }
 
