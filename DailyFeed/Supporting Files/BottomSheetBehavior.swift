ab5cad29a083cbfe227c73d78f40369cfa4e00e5
A guy <aguy@gmail.com>
2020-03-24 21:42:28 +0700

    prepare bottom sheet

---
+++ DailyFeed/Supporting Files/BottomSheetBehavior.swift
@@ -0,0 +1,79 @@
+//
+//  BottomSheetBehavior.swift
+//  DailyFeed
+//
+//  Created by Admin on 3/24/20.
+//  Copyright © 2020 trianz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import _SwiftUIKitOverlayShims
+
+class BottomSheetBehavior: UIDynamicBehavior {
+    let item                : UIDynamicItem
+    let itemBehavior        : UIDynamicItemBehavior
+    let attachmentBehavior          : UIAttachmentBehavior
+    private(set) var targetPoint : CGPoint               = .zero
+    private(set) var velocity    : CGPoint               = .zero
+
+    init(item dynamicItem: UIDynamicItem,
+         sheetConfiguration config: BottomSheetConfiguration,
+         onAnimationStep: ((_ minY: CGFloat) -> Void)? = nil) {
+        item = dynamicItem
+        itemBehavior = UIDynamicItemBehavior(items: [item])
+        attachmentBehavior  = UIAttachmentBehavior(item: item, attachedToAnchor: .zero)
+        super.init()
+
+        action = { [weak self] in
+            guard let view = self?.item as? UIView else {
+                return
+            }
+
+            var frame = view.frame
+            frame.size = config.sizeOf(sheetView: view)
+            view.frame = frame
+            view.layoutIfNeeded()
+            onAnimationStep?(frame.minY)
+        }
+
+        configureBehavior()
+        addBehaviors()
+    }
+    
+    func updateTargetPoint(_ point: CGPoint) {
+        targetPoint = point
+        attachmentBehavior.anchorPoint = targetPoint
+    }
+    
+    func updateVelocity(_ point: CGPoint) {
+        velocity = point
+        let currentVelocity = itemBehavior.linearVelocity(for: item)
+        let vDelta = velocity - currentVelocity
+        itemBehavior.addLinearVelocity(vDelta, for: item)
+    }
+}
+
+// MARK: Private Methods
+private extension BottomSheetBehavior {
+    func addBehaviors() {
+        addChildBehavior(attachmentBehavior)
+        addChildBehavior(itemBehavior)
+    }
+
+    func configureBehavior() {
+        configureAttachementBehavior()
+        configureItemBehavior()
+    }
+    
+    func configureAttachementBehavior() {
+        attachmentBehavior.frequency    = 3.5
+        attachmentBehavior.damping      = 0.4
+        attachmentBehavior.length       = 0
+    }
+    
+    func configureItemBehavior() {
+        itemBehavior.density    = 100
+        itemBehavior.resistance = 10
+    }
+}
---
+++ DailyFeed/Supporting Files/BottomSheetPanDirection.swift
@@ -0,0 +1,16 @@
+//
+//  BottomSheetPanDirection.swift
+//  DailyFeed
+//
+//  Created by Admin on 3/24/20.
+//  Copyright © 2020 trianz. All rights reserved.
+//
+
+import Foundation
+
+@objc
+public enum BottomSheetPanDirection: Int {
+    case
+    up,
+    down
+}
